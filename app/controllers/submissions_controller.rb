e36d50f20db90bfe90c236f7cf61aee552fa7104
githubdesktop <nat.morcos@gmail.com>
2017-12-10 20:32:51 -0500

    initial setup with a submissions model

---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,31 @@
+class SubmissionsController < ApplicationController
+
+  def index
+    @submissions = Submission.all
+  end
+
+  def show
+    @submission = Submission.find(params[:id])
+  end   
+
+  def new
+  end
+
+  def create
+    @submission = Submission.new(submission_params)
+
+    if @submission.save
+      redirect_to @submission
+    else
+      render :new
+    end
+    # render plain: params[:article].inspect
+  end
+
+  private
+
+  def submission_params
+    params.require(:submission).permit(:title, :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  
+  resources :submissions
+
+
+  root 'home#index'
+
   devise_for :users
+
 end
